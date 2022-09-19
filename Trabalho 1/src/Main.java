1831f804eb617436dae9ef53024eea9b7592cb3a
FelipeCardonaStudy <felipe.cardona@edu.pucrs.br>
2022-09-19 20:23:42 -0300

    feature/comunicação com o usuário

--- Trabalho 1/src/Main.java
+++ Trabalho 1/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
         Aplicativo app = new Aplicativo();
         try{
-            app.leArquivo("Testes");
+            System.out.println("Por favor, digite o nome do arquivo sem a extensão.");
+            String nomeArquivo = in.nextLine();
+            app.leArquivo(nomeArquivo);
         } catch (Exception e){
             System.err.println("Arquivo não encontrado.");
         }
