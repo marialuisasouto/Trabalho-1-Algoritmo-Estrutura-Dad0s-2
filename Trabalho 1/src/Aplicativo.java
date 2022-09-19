7bf01427596a8884bc7c1b0c25a9ca61ece9c5ce
FelipeCardonaStudy <felipe.cardona@edu.pucrs.br>
2022-09-19 16:55:57 -0300

    feature: adição do arquivo para teste e do método para leitura de arquivos

--- Trabalho 1/src/Aplicativo.java
+++ Trabalho 1/src/Aplicativo.java
@@ -1,2 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Aplicativo {
+    public void leArquivo(String nomeArquivo) throws IOException {
+        Path path = Paths.get(nomeArquivo + ".txt");
+        BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset());
+        String linha = "";
+        String separadorDeCampo = " ";
+
+        while((linha = br.readLine()) != null){
+
+        }
+    }
 }
--- Trabalho 1/src/Main.java
+++ Trabalho 1/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        Aplicativo app = new Aplicativo();
+        app.leArquivo("Testes");
+    }
 }
